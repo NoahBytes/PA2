40d20c86b0a87e49e9a42fc15c486ad7d2158c20
Noah Alavi <rqh12@txstate.edu>
2024-10-26 15:33:05 -0500

    Added run() logic handling and metric tracking variables

--- PA2.py
+++ PA2.py
@@ -11,6 +11,14 @@ t = -1
 eventQ = PriorityQueue()
 arrival_rate = int(sys.argv[1])
 service_time = float(sys.argv[2])
+totalTurnaround = -1 #Tracks total turnaround time, for averaging later.
+completedProcesses = -1
+busyTime = -1 #Time the CPU is busy
+
+#procsInQueue will track the number of processes in the queue.
+#Will need to be updated for each departure or arrival
+#Will hold tuples of type (# of processes in queue, time in queue)
+procsInQueue = []
 
 #Rough equivalent for the struct.
 #Type = 1 if arrival
@@ -36,13 +44,26 @@ def init():
     readyQueueCount = 0
     t = clock + exponential_dist(1/arrival_rate) #Generating first event time t
     eventQ.queue.clear()
+    totalTurnaround = 0
+    completedProcesses = 0
     sched_event(1, t)
 
-
+#Adds single event to priority queue, based on time and time
+#time dictates priority in queue.
 def sched_event(type: int, time: float):
     event = Event(type, time)
     eventQ.put((time, event))
 
+def run():
+    while (completedProcesses != 10000):
+        e = eventQ.get()
+        old_clock = clock
+        match e[1].type:
+            case 1:
+                arr_handler(e)
+            case -1:
+                dep_handler(e)
+
 init()
 while eventQ.qsize() != 0:
-    print (eventQ.get()) #FIXME testing
+    print (eventQ.get()[1].time) #FIXME testing
