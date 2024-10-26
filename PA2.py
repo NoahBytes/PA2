8729685a36c06751ad4a78022b7d379af74fde77
Noah Alavi <rqh12@txstate.edu>
2024-10-26 15:57:24 -0500

    Added arr_handler and dep_handler logic

--- PA2.py
+++ PA2.py
@@ -9,18 +9,17 @@ serverIdle = True
 readyQueueCount = -1
 t = -1
 eventQ = PriorityQueue()
-arrival_rate = int(sys.argv[1])
-service_time = float(sys.argv[2])
+arrivalRate = int(sys.argv[1])
+serviceTime = float(sys.argv[2])
 totalTurnaround = -1 #Tracks total turnaround time, for averaging later.
 completedProcesses = -1
 busyTime = -1 #Time the CPU is busy
 
-#procsInQueue will track the number of processes in the queue.
-#Will need to be updated for each departure or arrival
-#Will hold tuples of type (# of processes in queue, time in queue)
-procsInQueue = []
+#weightedProcsInQueue stores number of procsInQueue weighted by their time in queue
+#needs to be updated on each departure and arrival.
+weightedProcsInQueue = -1
 
-#Rough equivalent for the struct.
+#Essentially equivalent to the struct, but in python.
 #Type = 1 if arrival
 #Type = -1 if departure
 class Event():
@@ -42,10 +41,11 @@ def init():
     clock = 0
     serverIdle = True
     readyQueueCount = 0
-    t = clock + exponential_dist(1/arrival_rate) #Generating first event time t
+    t = clock + exponential_dist(1/arrivalRate) #Generating first event time t
     eventQ.queue.clear()
     totalTurnaround = 0
     completedProcesses = 0
+    weightedProcsInQueue = 0
     sched_event(1, t)
 
 #Adds single event to priority queue, based on time and time
@@ -54,16 +54,36 @@ def sched_event(type: int, time: float):
     event = Event(type, time)
     eventQ.put((time, event))
 
-def run():
-    while (completedProcesses != 10000):
-        e = eventQ.get()
-        old_clock = clock
-        match e[1].type:
-            case 1:
-                arr_handler(e)
-            case -1:
-                dep_handler(e)
+def arr_handler(e: Event, beginClock: float, endClock: float):
+    sched_event(1, clock + exponential_dist(1/arrivalRate))
+    weightedProcsInQueue += readyQueueCount * (endClock - beginClock)
+    if serverIdle:
+        serverIdle = False
+        sched_event(-1, clock + exponential_dist(serviceTime))
+    else: 
+        readyQueueCount += 1
 
-init()
+def dep_handler(e: Event, beginClock: float, endClock: float):
+    weightedProcsInQueue += readyQueueCount * (endClock - beginClock)
+    if readyQueueCount == 0:
+        serverIdle == True
+    else:
+        readyQueueCount -= 1
+        sched_event(-1, clock + exponential_dist(serviceTime))
+
+
+#run() handles the loops and logic of the simulation
+def run():
+    init()
+    while (completedProcesses != 10000):
+        e = eventQ.get()[1]
+        old_clock = clock
+        clock = e.time #update clock to time event begins
+        match e.type:
+            case 1:
+                arr_handler(e, old_clock, clock)
+            case -1:
+                dep_handler(e, old_clock, clock)
+
 while eventQ.qsize() != 0:
     print (eventQ.get()[1].time) #FIXME testing
